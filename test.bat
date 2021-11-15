47d03beef25fbd7798718562aa1a7d9ad560de61
Aleksa Heler <aleksaheler@gmail.com>
2021-11-15 13:08:57 +0100

    Update test.bat script
    Removed quotation marks from echo commands (not needed)

--- test.bat
+++ test.bat
@@ -5,32 +5,28 @@
 
 : Execute Model 0, Model 1 and Model 2
 @cd "Model 0"\Debug
-@echo "Running model 0"
+@echo Running model 0
 @"Model 0.exe" ..\..\Audio\speech_2ch.wav ..\out.wav -6 3
 @cd ..\..
 
 @cd "Model 1"\Debug
-@echo "Running model 1"
+@echo Running model 1
 @"Model 1.exe" ..\..\Audio\speech_2ch.wav ..\out.wav -6 3
 @cd ..\..
 
 @cd "Model 2"\Debug
-@echo "Running model 2"
+@echo Running model 2
 @"Model 2.exe" ..\..\Audio\speech_2ch.wav ..\out.wav -6 3
 @cd ..\..
-
-: cd model_2_3\Debug
-: "filter Model2.exe"
-: cd ..\..
 
 : cd model_2_3\
 : c:\CirrusDSP\bin\cmdline_simulator.exe -project SimulatorConfigurationTemp.sbr -max_cycles 1000000
 : cd ..
 
 : Generate new logs
-@echo "Results 0vs1 (in Docs/compare_model0_vs_model1.txt):"
+@echo Results 0vs1 (in Docs/compare_model0_vs_model1.txt):
 @Audio\PCMCompare.exe "Model 0/out.wav" "Model 1/out.wav" -b16 2> Docs\compare_model0_vs_model1.txt
-@echo "Results 1vs2 (in Docs/compare_model1_vs_model2.txt):"
+@echo Results 1vs2 (in Docs/compare_model1_vs_model2.txt):
 @Audio\PCMCompare.exe "Model 1/out.wav" "Model 2/out.wav" -b16 2> Docs\compare_model1_vs_model2.txt
 
 pause
